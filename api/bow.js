4f91431220879d53fc133d24da2ee0f494b50199
dmarcus-wire <dmarcus@redhat.com>
2025-12-23 14:01:09 -0700

    Change 'Access-Control-Allow-Origin', '*' to your exact frontend origin

--- api/bow.js
+++ api/bow.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // CORS headers for browser requests
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://arrow-forge-ai-git-ai-bow-lookup-davids-projects-0e631f5c.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
